cc2a772b4a516d132337e86a381f6a3c89e3578d
Shakim Harris <amenraprod@gmail.com>
2024-10-25 14:55:43 -0400

    rendered individual veh data

---
+++ src/vehicleDisplay.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import vehicleItem from './vehicleItem';
+import VehicleItem from './vehicleItem';
+export default function VehicleDisplay () {
+    return (
+        <>
+        <VehicleItem manufacturer={''} color={''} vehicleMake={''} vin={''} vrm={''} />
+        <VehicleItem manufacturer={''} color={''} vehicleMake={''} vin={''} vrm={''} />
+        <VehicleItem manufacturer={''} color={''} vehicleMake={''} vin={''} vrm={''} />
+        <VehicleItem manufacturer={''} color={''} vehicleMake={''} vin={''} vrm={''} />
+        <VehicleItem manufacturer={''} color={''} vehicleMake={''} vin={''} vrm={''} /></>
+        
+    )
+};
