06c6dd399f0e7f67f102a002e8ad85d0a63c08bf
Shakim Harris <amenraprod@gmail.com>
2024-10-25 14:55:32 -0400

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
+import VehicleDisplay from './vehicleDisplay'
 import './App.css';
+import { useEffect, useState } from 'react';
+import { faker } from '@faker-js/faker';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/vehicle">Vehicle Display Page</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/vehicle" element={<VehicleDisplay />}></Route>
+        </Routes>
+     </Router>
     </div>
   );
 }
